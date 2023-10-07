cfacb0f66088898580d4683931bd079c48e4aa53
rayanadir <rayan.dahmena@gmail.com>
2023-10-07 23:04:09 +0200

    add CommentMapper

---
+++ back/src/main/java/com/openclassrooms/mddapi/mapper/CommentMapper.java
@@ -0,0 +1,32 @@
+package com.openclassrooms.mddapi.mapper;
+
+import com.openclassrooms.mddapi.dto.CommentDTO;
+import com.openclassrooms.mddapi.model.Comment;
+import com.openclassrooms.mddapi.model.Post;
+import com.openclassrooms.mddapi.service.CommentService;
+import com.openclassrooms.mddapi.service.UserService;
+import org.hibernate.engine.internal.Collections;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+
+@Mapper(componentModel = "spring", uses={CommentService.class}, imports = {Arrays.class, Collections.class, Post.class})
+public abstract class CommentMapper implements EntityMapper<CommentDTO, Comment> {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private UserService userService;
+
+    @Mappings({
+            @Mapping(source = "content", target = "content"),
+            @Mapping(source = "created_at", target = "created_at"),
+            @Mapping(source = "user.username", target = "username"),
+            @Mapping(source = "post.id", target = "id"),
+    })
+    public abstract CommentDTO toDTO(Comment comment);
+}
