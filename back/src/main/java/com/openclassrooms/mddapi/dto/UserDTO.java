35cb3e3a4259f9a900423b327cc22bc2e9e78569
rayanadir <rayan.dahmena@gmail.com>
2023-10-07 23:08:04 +0200

    add getters/setters

--- back/src/main/java/com/openclassrooms/mddapi/dto/UserDTO.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/UserDTO.java
@@ -32,4 +32,48 @@ public class UserDTO {
     @NonNull
     private LocalDateTime created_at;
 
+    @NonNull
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(@NonNull Long id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NonNull String email) {
+        this.email = email;
+    }
+
+    @NonNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NonNull String username) {
+        this.username = username;
+    }
+
+    @NonNull
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NonNull String password) {
+        this.password = password;
+    }
+
+    @NonNull
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(@NonNull LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
 }
