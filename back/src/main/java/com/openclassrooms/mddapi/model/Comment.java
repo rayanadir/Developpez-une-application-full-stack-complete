71e9cfcf5e2039e3992b7a927c09256a3566973f
rayanadir <rayan.dahmena@gmail.com>
2023-10-07 19:31:37 +0200

    add attributes

--- back/src/main/java/com/openclassrooms/mddapi/model/Comment.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/Comment.java
@@ -13,10 +13,16 @@ public class Comment {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "comment_id")
 	private Long id;
-	
-	// TODO : to finish...
+
+	@Column(name="post_id")
+	private Post post;
+
+	@Column(name= "post_id")
+	private User user;
+
+	@Column(name = "content")
+	private String content;
 
 	public Long getId() {
 		return id;
@@ -24,6 +30,30 @@ public class Comment {
 
 	public void setId(Long id) {
 		this.id = id;
-	}	
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 	
 }
