c08388f4eb04cdda34a5c8b5310c97b7ccf927f6
rayanadir <rayan.dahmena@gmail.com>
2023-10-07 19:32:39 +0200

    add findByPost

--- back/src/main/java/com/openclassrooms/mddapi/repository/CommentRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/CommentRepository.java
@@ -1,11 +1,15 @@
 package com.openclassrooms.mddapi.repository;
 
+import com.openclassrooms.mddapi.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.mddapi.model.Comment;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    Optional<List<Comment>> findByPost(Post post);
 }
