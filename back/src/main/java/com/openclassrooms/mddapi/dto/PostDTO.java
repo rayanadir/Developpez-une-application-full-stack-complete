7aac9ce6b1654c8ccb262018b77d4625de521fd7
rayanadir <rayan.dahmena@gmail.com>
2023-10-07 23:06:37 +0200

    add getters/setters

--- back/src/main/java/com/openclassrooms/mddapi/dto/PostDTO.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/PostDTO.java
@@ -18,7 +18,7 @@ public class PostDTO {
     private Long topic_id;
 
     @NonNull
-    private Long user_id;
+    private Long author_id;
 
     @NonNull
     private String title;
@@ -31,4 +31,67 @@ public class PostDTO {
 
     @NonNull
     private LocalDateTime updated_at;
+
+    @NonNull
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(@NonNull Long id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public Long getTopic_id() {
+        return topic_id;
+    }
+
+    public void setTopic_id(@NonNull Long topic_id) {
+        this.topic_id = topic_id;
+    }
+
+    @NonNull
+    public Long getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(@NonNull Long author_id) {
+        this.author_id = author_id;
+    }
+
+    @NonNull
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NonNull String title) {
+        this.title = title;
+    }
+
+    @NonNull
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(@NonNull String content) {
+        this.content = content;
+    }
+
+    @NonNull
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(@NonNull LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    @NonNull
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(@NonNull LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
 }
