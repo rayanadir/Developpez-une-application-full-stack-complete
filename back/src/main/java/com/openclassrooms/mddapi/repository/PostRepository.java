216cca1f5033ac80171619221dccdf2701299497
rayanadir <rayan.dahmena@gmail.com>
2023-10-07 19:34:54 +0200

    add findByTopic

--- back/src/main/java/com/openclassrooms/mddapi/repository/PostRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/PostRepository.java
@@ -1,11 +1,15 @@
 package com.openclassrooms.mddapi.repository;
 
+import com.openclassrooms.mddapi.model.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.mddapi.model.Post;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
-
+    Optional<List<Post>> findByTopic(Topic topic);
 }
