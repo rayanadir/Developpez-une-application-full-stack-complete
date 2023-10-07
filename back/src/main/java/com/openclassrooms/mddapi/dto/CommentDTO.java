7132eed7cad1250795c870979cefb1d90091f709
rayanadir <rayan.dahmena@gmail.com>
2023-10-07 23:05:38 +0200

    add getters/setters

--- back/src/main/java/com/openclassrooms/mddapi/dto/CommentDTO.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/CommentDTO.java
@@ -23,4 +23,40 @@ public class CommentDTO {
 
     @NonNull
     private LocalDateTime created_at;
+
+    @NonNull
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(@NonNull Long id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NonNull String username) {
+        this.username = username;
+    }
+
+    @NonNull
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(@NonNull String content) {
+        this.content = content;
+    }
+
+    @NonNull
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(@NonNull LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
 }
