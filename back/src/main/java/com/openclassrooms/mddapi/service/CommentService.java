fba182393f561fe0aa36a8275f7ce044ac2f049d
rayanadir <rayan.dahmena@gmail.com>
2023-10-07 19:49:45 +0200

    add CommentService + methods

---
+++ back/src/main/java/com/openclassrooms/mddapi/service/CommentService.java
@@ -0,0 +1,23 @@
+package com.openclassrooms.mddapi.service;
+
+import com.openclassrooms.mddapi.model.Comment;
+import com.openclassrooms.mddapi.model.Post;
+import com.openclassrooms.mddapi.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public Comment create(Comment comment){
+        return this.commentRepository.save(comment);
+    }
+    public Optional<List<Comment>> findByPost(Post post){
+        return this.commentRepository.findByPost(post);
+    }
+}
