51223b28cc395234e6edfa4af3e1ffc4085013aa
rayanadir <rayan.dahmena@gmail.com>
2023-10-07 23:07:00 +0200

    add getters/setters

--- back/src/main/java/com/openclassrooms/mddapi/dto/TopicDTO.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/TopicDTO.java
@@ -18,4 +18,31 @@ public class TopicDTO {
 
     @NonNull
     private String name;
+
+    @NonNull
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(@NonNull Long id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@NonNull String description) {
+        this.description = description;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
 }
