3c15cd426486e048015aedea83f8992a3f8b561e
rayanadir <rayan.dahmena@gmail.com>
2023-10-07 19:37:44 +0200

    update TopicController

--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
@@ -2,26 +2,26 @@ package com.openclassrooms.mddapi.controller;
 
 import java.util.List;
 
+import com.openclassrooms.mddapi.service.TopicService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.mddapi.model.Topic;
-import com.openclassrooms.mddapi.service.ITopicService;
 
 @RestController
 @RequestMapping("/topic")
 public class TopicController {
 	
-	private ITopicService topicService;
+	private TopicService topicService;
 	
-	public TopicController(ITopicService topicService) {
+	public TopicController(TopicService topicService) {
 		this.topicService = topicService;		
 	}
 
 	@GetMapping
 	public List<Topic> getTopics() {
-		return topicService.getTopics();
+		return topicService.findAllTopics();
 	}
 	
 	
