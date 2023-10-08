eb36b0054ffd906d48c143fd08ea051681c1bc21
rayanadir <rayan.dahmena@gmail.com>
2023-10-08 18:49:44 +0200

    change method names + add find subscriptions by user and topics

--- back/src/main/java/com/openclassrooms/mddapi/service/SubscriptionService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/SubscriptionService.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.mddapi.service;
 
 import com.openclassrooms.mddapi.model.Subscription;
+import com.openclassrooms.mddapi.model.Topic;
 import com.openclassrooms.mddapi.model.User;
 import com.openclassrooms.mddapi.repository.SubscriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +16,11 @@ public class SubscriptionService {
     @Autowired
     private SubscriptionRepository subscriptionRepository;
 
-    private Subscription create(Subscription subscription){
+    public Subscription subscribe(Subscription subscription){
         return this.subscriptionRepository.save(subscription);
     }
 
-    public void delete(Long id){
+    public void unsubscribe(Long id){
         this.subscriptionRepository.deleteById(id);
     }
 
@@ -30,6 +31,8 @@ public class SubscriptionService {
     public Subscription findById(Long id){
         return this.subscriptionRepository.findById(id).orElse(null);
     }
+
+    public Optional<Subscription> findByUserAndTopic(User user, Topic topic){ return this.subscriptionRepository.findByUserAndTopic(user,topic); }
 
     public Optional<List<Subscription>> findByUser(User user){
         return this.subscriptionRepository.findByUser(user);
