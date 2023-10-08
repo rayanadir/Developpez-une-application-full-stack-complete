f38be147ebe61b3083519a36e2f8bdd58ac5a481
rayanadir <rayan.dahmena@gmail.com>
2023-10-08 23:29:12 +0200

    add get topics

--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
@@ -1,8 +1,13 @@
 package com.openclassrooms.mddapi.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.openclassrooms.mddapi.dto.TopicDTO;
+import com.openclassrooms.mddapi.mapper.TopicMapper;
 import com.openclassrooms.mddapi.service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +15,33 @@ import org.springframework.web.bind.annotation.RestController;
 import com.openclassrooms.mddapi.model.Topic;
 
 @RestController
-@RequestMapping("/topic")
+@RequestMapping("/api/topic")
 public class TopicController {
-	
+
+	@Autowired
 	private TopicService topicService;
-	
+
+	@Autowired
+	private TopicMapper topicMapper;
+
 	public TopicController(TopicService topicService) {
 		this.topicService = topicService;		
 	}
 
 	@GetMapping
-	public List<Topic> getTopics() {
-		return topicService.findAllTopics();
+	public ResponseEntity<?> getTopics() {
+		try{
+			List<Topic> topics = topicService.findAllTopics();
+			if(topics.isEmpty())
+				return ResponseEntity.ok().build();
+			List<TopicDTO> list = new ArrayList<>();
+			for(Topic topic: topics){
+				list.add(this.topicMapper.toDto(topic));
+			}
+			return ResponseEntity.ok().body(list);
+		}catch(NumberFormatException e){
+			return ResponseEntity.badRequest().build();
+		}
 	}
 	
 	
