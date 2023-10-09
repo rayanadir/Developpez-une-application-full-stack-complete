6ffe21b28154004331a30500b8c48994e9bca429
rayanadir <rayan.dahmena@gmail.com>
2023-10-09 12:35:50 +0200

    add @Autowired

--- back/src/main/java/com/openclassrooms/mddapi/service/TopicService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/TopicService.java
@@ -2,6 +2,7 @@ package com.openclassrooms.mddapi.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.openclassrooms.mddapi.model.Topic;
@@ -10,6 +11,7 @@ import com.openclassrooms.mddapi.repository.TopicRepository;
 @Service
 public class TopicService {
 
+	@Autowired
 	private TopicRepository topicRepository;
 	
 	public TopicService(TopicRepository topicRepository) {
