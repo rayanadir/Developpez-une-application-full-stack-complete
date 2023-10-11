e3de7c2d5d44d4c922d8531d0d8d1c59244e5e7f
rayanadir <rayan.dahmena@gmail.com>
2023-10-11 22:01:38 +0200

    declare and import modules

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopicComponent } from './topic/topic.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopicComponent
+    TopicComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
