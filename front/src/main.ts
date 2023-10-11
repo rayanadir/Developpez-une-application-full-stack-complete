4479b8c01436a8798f404ba850ced6acbe0364e3
rayanadir <rayan.dahmena@gmail.com>
2023-10-11 21:50:50 +0200

    add imports

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,6 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import '@angular/compiler';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +14,9 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  bootstrapApplication(AppComponent, {
+    providers: [
+      provideAnimations(),
+    ]
+  })
