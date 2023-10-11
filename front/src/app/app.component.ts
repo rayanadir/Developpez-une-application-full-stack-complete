b0fcc4e4f5fddea96915f6df305f2a76d7efb6de
rayanadir <rayan.dahmena@gmail.com>
2023-10-11 21:58:46 +0200

    subscribe current url

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //standalone: true,
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
+  showHeader: boolean = true;
+
+  constructor(public router: Router){
+    this.router.events.subscribe((event:any) => {
+      if(event instanceof NavigationEnd){
+        if(router.url == "/welcome"){
+          this.showHeader=false;
+        }else{
+          this.showHeader=true;
+        }
+      }
+    })
+    
+  }
+
+  ngOnInit(): void {
+    
+    
+  }
+
+
 }
