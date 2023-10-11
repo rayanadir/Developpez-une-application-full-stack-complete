e7f335a8b2f7568d53114b5c5b79b6862bce7b3c
rayanadir <rayan.dahmena@gmail.com>
2023-10-11 21:53:09 +0200

    add header component

---
+++ front/src/app/components/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { ResponsiveService } from 'src/app/services/responsive.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  currentBreakpoint:"desktop" | "tablet" | "phone" | undefined;
+  public responsiveSubscription! : Subscription;
+  public screenHeight: any;
+  public mainTagHeight: any;
+
+  constructor(public location: Location,  private responsiveService: ResponsiveService) { }
+
+  ngOnInit(): void {
+    /**
+     * Observe current window format : "desktop" | "tablet" | "phone" | undefined
+     */
+    this.responsiveSubscription = this.responsiveService.observeBreakpoint().subscribe(() => {
+      this.currentBreakpoint = this.responsiveService.breakpointChanged();
+    });
+  }
+
+  navigateBack(){
+    this.location.back();
+  }
+
+}
