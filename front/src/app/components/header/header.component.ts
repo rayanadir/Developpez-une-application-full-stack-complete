98a779bf5211a1b5f29de70e1fc00a40b6aca762
rayanadir <rayan.dahmena@gmail.com>
2023-10-12 17:17:40 +0200

    commit

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { ResponsiveService } from 'src/app/services/responsive.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
 
