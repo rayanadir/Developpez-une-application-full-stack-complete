8595dd23f94caf8a90638b637d3ac82550d20e25
rayanadir <rayan.dahmena@gmail.com>
2023-10-12 17:12:19 +0200

    add modules

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,17 +5,36 @@ import { AppComponent } from './app.component';
 import { TopicComponent } from './topic/topic.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+const materialModule = [MatButtonModule, MatDividerModule, MatIconModule, MatFormFieldModule, MatInputModule,MatCardModule]
 
 @NgModule({
   declarations: [
     AppComponent,
     TopicComponent,
     HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    ...materialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
