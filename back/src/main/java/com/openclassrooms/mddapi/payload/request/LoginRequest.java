9c6d8af5c3b3a56804053a616d560929dfe8bf4d
rayanadir <rayan.dahmena@gmail.com>
2023-10-12 12:21:47 +0200

    add annotations

--- back/src/main/java/com/openclassrooms/mddapi/payload/request/LoginRequest.java
+++ back/src/main/java/com/openclassrooms/mddapi/payload/request/LoginRequest.java
@@ -1,7 +1,19 @@
 package com.openclassrooms.mddapi.payload.request;
 
+import lombok.NonNull;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
 public class LoginRequest {
+
+    @NonNull
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}", flags = Pattern.Flag.CASE_INSENSITIVE)
     private String email;
+
+    @NonNull
+    @Length(min=8)
     private String password;
 
     public String getEmail() {
