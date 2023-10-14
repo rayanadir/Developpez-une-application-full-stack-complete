a307cf2a03b4e25e68a9523bd8dc198e069b2c79
rayanadir <rayan.dahmena@gmail.com>
2023-10-14 23:59:04 +0200

    add posts component

---
+++ front/src/app/pages/posts/posts.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ResponsiveService } from 'src/app/services/responsive.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent implements OnInit, OnDestroy {
+
+  currentBreakpoint:"desktop" | "tablet" | "phone" | undefined;
+  public responsiveSubscription! : Subscription;
+
+  
+
+  Arr = Array; //Array type captured in a variable
+  num:number = 5;
+
+  constructor(private responsiveService: ResponsiveService) { }
+
+  ngOnInit(): void {
+    /**
+     * Observe current window format : "desktop" | "tablet" | "phone" | undefined
+     */
+    this.responsiveSubscription = this.responsiveService.observeBreakpoint().subscribe(() => {
+      this.currentBreakpoint = this.responsiveService.breakpointChanged();
+      if(this.currentBreakpoint!=undefined){
+        document.querySelector('main')?.setAttribute('format', this.currentBreakpoint);
+        document.querySelector('.create-filter')?.setAttribute('format', this.currentBreakpoint);
+        document.querySelector('.posts')?.setAttribute('format', this.currentBreakpoint);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+      this.responsiveSubscription.unsubscribe();
+  }
+}
