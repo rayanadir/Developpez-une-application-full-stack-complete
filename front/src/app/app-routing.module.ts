87cc90738b858902f0d7324c788e0e3d56d28843
rayanadir <rayan.dahmena@gmail.com>
2023-10-15 18:02:20 +0200

    add routes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,13 +5,45 @@ import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { PostsComponent } from './pages/posts/posts.component';
+import { TopicsComponent } from './pages/topics/topics.component';
+import { AccountComponent } from './pages/account/account.component';
 
-const routes : Routes = [
-  {path:"", redirectTo:"welcome", pathMatch:"full"},
-  {title: "Welcome", path: "welcome", component: WelcomeComponent},
-  {title: "Login", path:"login", component: LoginComponent},
-  {title: "Register", path:"register", component: RegisterComponent},
-  {title: "Posts", path:"posts", component: PostsComponent}
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "welcome",
+    pathMatch: "full"
+  },
+  {
+    title: "Welcome",
+    path: "welcome",
+    component: WelcomeComponent
+  },
+  { 
+    title: "Login",
+    path: "login",
+    component: LoginComponent 
+  },
+  { 
+    title: "Register",
+    path: "register",
+    component: RegisterComponent 
+  },
+  { 
+    title: "Posts",
+    path: "posts",
+    component: PostsComponent
+  },
+  { 
+    title: "Topics",
+    path: "topics",
+    component: TopicsComponent
+  },
+  {
+    title: "Account",
+    path: "account",
+    component: AccountComponent
+  }
 ];
 
 @NgModule({
