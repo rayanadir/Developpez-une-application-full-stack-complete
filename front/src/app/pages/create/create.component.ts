bcdc3e123b46a7f60faeef83e05b5e34311dc285
rayanadir <rayan.dahmena@gmail.com>
2023-10-17 13:52:17 +0200

    add create post component

---
+++ front/src/app/pages/create/create.component.ts
@@ -0,0 +1,46 @@
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ResponsiveService } from 'src/app/services/responsive.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss'],
+})
+export class CreateComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  currentBreakpoint:"desktop" | "tablet" | "phone" | undefined;
+  public responsiveSubscription! : Subscription;
+
+  constructor(private responsiveService: ResponsiveService, private router: Router) { }
+
+  ngOnInit(): void {
+    /**
+     * Observe current window format : "desktop" | "tablet" | "phone" | undefined
+     */
+    this.responsiveSubscription = this.responsiveService.observeBreakpoint().subscribe(() => {
+      this.currentBreakpoint = this.responsiveService.breakpointChanged();
+      if(this.currentBreakpoint!=undefined){
+        document.querySelector('.main-create')?.setAttribute('format', this.currentBreakpoint);
+        document.querySelector('.card-header')?.setAttribute('format', this.currentBreakpoint);
+        document.querySelector('.create-content')?.setAttribute('format', this.currentBreakpoint);
+        document.querySelector('.title')?.setAttribute('format', this.currentBreakpoint);
+        document.querySelector('.back')?.setAttribute('format', this.currentBreakpoint);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+      
+  }
+
+  ngAfterViewInit(): void {
+      
+  }
+
+  back(){
+    this.router.navigate(['/posts'])
+  }
+
+}
