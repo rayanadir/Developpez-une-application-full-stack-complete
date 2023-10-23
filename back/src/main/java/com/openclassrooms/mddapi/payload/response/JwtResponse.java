34c864e9300ed70d3420b5afd656b42eb2f46c4b
rayanadir <rayan.dahmena@gmail.com>
2023-10-23 21:55:58 +0200

    change JwtResponse constructor

--- back/src/main/java/com/openclassrooms/mddapi/payload/response/JwtResponse.java
+++ back/src/main/java/com/openclassrooms/mddapi/payload/response/JwtResponse.java
@@ -1,17 +1,20 @@
 package com.openclassrooms.mddapi.payload.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class JwtResponse {
     private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
 
-    public JwtResponse(String token){
-        this.token=token;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
+    public JwtResponse(String token, Long id, String username){
         this.token = token;
+        this.id = id;
+        this.username = username;
     }
+
 }
