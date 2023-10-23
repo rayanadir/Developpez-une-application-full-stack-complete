d1244ebf44d8fcea0eb196ae21bc5f5113abf0b1
rayanadir <rayan.dahmena@gmail.com>
2023-10-23 22:01:34 +0200

    add email

--- back/src/main/java/com/openclassrooms/mddapi/payload/response/JwtResponse.java
+++ back/src/main/java/com/openclassrooms/mddapi/payload/response/JwtResponse.java
@@ -10,11 +10,13 @@ public class JwtResponse {
     private String type = "Bearer";
     private Long id;
     private String username;
+    private String email;
 
-    public JwtResponse(String token, Long id, String username){
+    public JwtResponse(String token, Long id, String username, String email){
         this.token = token;
         this.id = id;
         this.username = username;
+        this.email = email;
     }
 
 }
