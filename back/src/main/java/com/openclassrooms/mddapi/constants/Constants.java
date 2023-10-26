ebac6d333ff4b828caba320b42a662cc7acaf91a
rayanadir <rayan.dahmena@gmail.com>
2023-10-26 16:04:23 +0200

    add constants

---
+++ back/src/main/java/com/openclassrooms/mddapi/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.openclassrooms.mddapi.constants;
+
+public class Constants {
+    public static final String SECRET = "SECRET_KEY";
+    public static final int EXPIRATION_TIME = 1000*60*15;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String BASE_URL = "http://localhost:4200/";
+}
