f083ee8b7586963abc64c22cba7523523f3168cb
rayanadir <rayan.dahmena@gmail.com>
2023-10-26 18:13:31 +0200

    add javadoc

--- back/src/main/java/com/openclassrooms/mddapi/security/jwt/AuthTokenFilter.java
+++ back/src/main/java/com/openclassrooms/mddapi/security/jwt/AuthTokenFilter.java
@@ -1,14 +1,13 @@
 package com.openclassrooms.mddapi.security.jwt;
 
-import com.openclassrooms.mddapi.security.services.UserDetailsServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.openclassrooms.mddapi.service.UserService;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.util.StringUtils;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -16,44 +15,49 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
+import static com.openclassrooms.mddapi.constants.Constants.HEADER_STRING;
+import static com.openclassrooms.mddapi.constants.Constants.TOKEN_PREFIX;
+
+/**
+ * AuthTokenFilter class
+ */
+@Component
 public class AuthTokenFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtUtils jwtUtils;
 
     @Autowired
-    private UserDetailsServiceImpl userDetailsService;
+    private UserService userService;
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
-
+    /**
+     * Set authentication token by user context, does filter with http request & response
+     * @param request
+     * @param response
+     * @param filterChain
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        try{
-            String jwt = parseJwt(request);
-            if(jwt != null && jwtUtils.validateJwtToken(jwt)) {
-                String username = jwtUtils.getUserNameFromJwtToken(jwt);
-
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(
-                                userDetails,
-                                null,
-                                userDetails.getAuthorities()
-                        );
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+        if(SecurityContextHolder.getContext().getAuthentication() == null){
+            String authorization = request.getHeader(HEADER_STRING);
+            if(authorization != null && authorization.startsWith(TOKEN_PREFIX)){
+                String token = authorization.substring(7);
+                Claims claims = this.jwtUtils.getClaims(token);
+                if(claims.getExpiration().after((new Date()))){
+                    String name = claims.getSubject();
+                    UserDetails userDetails = this.userService.loadUserByUsername(name);
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
             }
-        }catch (Exception e){
-            logger.error("Cannot set user authentication: {}", e);
         }
+        filterChain.doFilter(request,response);
     }
 
-    private String parseJwt(HttpServletRequest request){
-        String headerAuth = request.getHeader("Authorization");
-        if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer "))
-            return headerAuth.substring(7, headerAuth.length());
-        return null;
-    }
+
 }
