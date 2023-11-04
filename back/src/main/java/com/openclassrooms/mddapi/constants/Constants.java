ca2a5ff357fc227c5e3480c930d33ec7465ead48
rayanadir <rayan.dahmena@gmail.com>
2023-11-04 21:52:57 +0100

    change jwt lifetime

--- back/src/main/java/com/openclassrooms/mddapi/constants/Constants.java
+++ back/src/main/java/com/openclassrooms/mddapi/constants/Constants.java
@@ -2,7 +2,7 @@ package com.openclassrooms.mddapi.constants;
 
 public class Constants {
     public static final String SECRET = "SECRET_KEY";
-    public static final int EXPIRATION_TIME = 1000*60*60;
+    public static final int EXPIRATION_TIME = 1000*60*60*24*3;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+!*?=-])(?=\\S+$).{8,}$";
