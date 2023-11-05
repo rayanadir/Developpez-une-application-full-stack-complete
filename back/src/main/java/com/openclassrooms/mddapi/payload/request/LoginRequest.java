dfa68cfe8386bcef7c7656b186dbe4d22f11d1fd
rayanadir <rayan.dahmena@gmail.com>
2023-11-05 14:22:49 +0100

    getter/setter (@Data)

--- back/src/main/java/com/openclassrooms/mddapi/payload/request/LoginRequest.java
+++ back/src/main/java/com/openclassrooms/mddapi/payload/request/LoginRequest.java
@@ -20,20 +20,4 @@ public class LoginRequest {
     @NonNull
     @Length(min=8)
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
