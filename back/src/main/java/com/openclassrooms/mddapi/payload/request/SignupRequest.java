8dffb4d9f63aef72f3c184d6eb081102f25901fb
rayanadir <rayan.dahmena@gmail.com>
2023-11-05 14:21:52 +0100

    change username -> name

--- back/src/main/java/com/openclassrooms/mddapi/payload/request/SignupRequest.java
+++ back/src/main/java/com/openclassrooms/mddapi/payload/request/SignupRequest.java
@@ -18,37 +18,11 @@ public class SignupRequest {
 
     @NonNull
     @Length(min=3)
-    private String username;
+    private String name;
 
     @NonNull
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+!*?=-])(?=\\S+$).{8,}$")
     @Length(min = 8, max = 40)
     private String password;
 
-    @NonNull
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(@NonNull String email) {
-        this.email = email;
-    }
-
-    @NonNull
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(@NonNull String username) {
-        this.username = username;
-    }
-
-    @NonNull
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(@NonNull String password) {
-        this.password = password;
-    }
 }
